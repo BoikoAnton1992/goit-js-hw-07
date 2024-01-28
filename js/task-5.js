4c81fffc07bb962f42c319138eb023c1ce33c966
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-01-28 15:13:47 +0200

    Task 5 + task 6 redy

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+function changeBackgroundColor() {
+  const newColor = getRandomHexColor();
+
+
+  body.style.backgroundColor = newColor;
+
+  colorSpan.textContent = newColor;
+}
+
+changeColorButton.addEventListener('click', changeBackgroundColor);
